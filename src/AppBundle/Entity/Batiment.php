59f7968c065383a72617c6ad1a87b6afa7bad64a
Cocdesbois <corentin.fougeret@laposte.net>
2017-02-23 19:29:39 +0100

    Ajout Entity
    Ajout de l'Entity Batiment pour test.

---
+++ src/AppBundle/Entity/Batiment.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Batiment
+ *
+ * @ORM\Table(name="batiment")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BatimentRepository")
+ */
+class Batiment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=100)
+     */
+    private $libelle;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     *
+     * @return Batiment
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+}
+
