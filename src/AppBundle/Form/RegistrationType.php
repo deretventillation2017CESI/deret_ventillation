e5605a048db49f0238abf00aafeda6477c66e07b
FabienBrG <waofab36@hotmail.fr>
2017-04-07 15:46:04 +0200

    Debut FOSUserBundle (Loic & Fabien)

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,37 @@
+<?php
+// src/AppBundle/Form/RegistrationType.php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom');
+        $builder->add('prenom');
+        $builder->add('statut');
+        $builder->add('identifiantIGE');
+        $builder->add('responsabe');
+        $builder->add('nbHeureTheoriqueSession');
+        $builder->add('dateCreation');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
