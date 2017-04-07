0f93d58e8e732fd2a0addc8bd9f12af627b33c69
loic <bezzaouya.loic@hotmail.fr>
2017-04-07 11:30:11 +0200

    CRUD
    Crud Batiment,Poste,Dossier

---
+++ src/AppBundle/Controller/BatimentController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Batiment;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Batiment controller.
+ *
+ * @Route("batiment")
+ */
+class BatimentController extends Controller
+{
+    /**
+     * Lists all batiment entities.
+     *
+     * @Route("/", name="batiment_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $batiments = $em->getRepository('AppBundle:Batiment')->findAll();
+
+        return $this->render('batiment/index.html.twig', array(
+            'batiments' => $batiments,
+        ));
+    }
+
+    /**
+     * Creates a new batiment entity.
+     *
+     * @Route("/new", name="batiment_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $batiment = new Batiment();
+        $form = $this->createForm('AppBundle\Form\BatimentType', $batiment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($batiment);
+            $em->flush();
+
+            return $this->redirectToRoute('batiment_show', array('id' => $batiment->getId()));
+        }
+
+        return $this->render('batiment/new.html.twig', array(
+            'batiment' => $batiment,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a batiment entity.
+     *
+     * @Route("/{id}", name="batiment_show")
+     * @Method("GET")
+     */
+    public function showAction(Batiment $batiment)
+    {
+        $deleteForm = $this->createDeleteForm($batiment);
+
+        return $this->render('batiment/show.html.twig', array(
+            'batiment' => $batiment,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing batiment entity.
+     *
+     * @Route("/{id}/edit", name="batiment_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Batiment $batiment)
+    {
+        $deleteForm = $this->createDeleteForm($batiment);
+        $editForm = $this->createForm('AppBundle\Form\BatimentType', $batiment);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('batiment_edit', array('id' => $batiment->getId()));
+        }
+
+        return $this->render('batiment/edit.html.twig', array(
+            'batiment' => $batiment,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a batiment entity.
+     *
+     * @Route("/{id}", name="batiment_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Batiment $batiment)
+    {
+        $form = $this->createDeleteForm($batiment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($batiment);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('batiment_index');
+    }
+
+    /**
+     * Creates a form to delete a batiment entity.
+     *
+     * @param Batiment $batiment The batiment entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Batiment $batiment)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('batiment_delete', array('id' => $batiment->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Controller/DossierController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Dossier;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Dossier controller.
+ *
+ * @Route("dossier")
+ */
+class DossierController extends Controller
+{
+    /**
+     * Lists all dossier entities.
+     *
+     * @Route("/", name="dossier_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $dossiers = $em->getRepository('AppBundle:Dossier')->findAll();
+
+        return $this->render('dossier/index.html.twig', array(
+            'dossiers' => $dossiers,
+        ));
+    }
+
+    /**
+     * Creates a new dossier entity.
+     *
+     * @Route("/new", name="dossier_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $dossier = new Dossier();
+        $form = $this->createForm('AppBundle\Form\DossierType', $dossier);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($dossier);
+            $em->flush();
+
+            return $this->redirectToRoute('dossier_show', array('id' => $dossier->getId()));
+        }
+
+        return $this->render('dossier/new.html.twig', array(
+            'dossier' => $dossier,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a dossier entity.
+     *
+     * @Route("/{id}", name="dossier_show")
+     * @Method("GET")
+     */
+    public function showAction(Dossier $dossier)
+    {
+        $deleteForm = $this->createDeleteForm($dossier);
+
+        return $this->render('dossier/show.html.twig', array(
+            'dossier' => $dossier,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing dossier entity.
+     *
+     * @Route("/{id}/edit", name="dossier_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Dossier $dossier)
+    {
+        $deleteForm = $this->createDeleteForm($dossier);
+        $editForm = $this->createForm('AppBundle\Form\DossierType', $dossier);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('dossier_edit', array('id' => $dossier->getId()));
+        }
+
+        return $this->render('dossier/edit.html.twig', array(
+            'dossier' => $dossier,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a dossier entity.
+     *
+     * @Route("/{id}", name="dossier_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Dossier $dossier)
+    {
+        $form = $this->createDeleteForm($dossier);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($dossier);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('dossier_index');
+    }
+
+    /**
+     * Creates a form to delete a dossier entity.
+     *
+     * @param Dossier $dossier The dossier entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Dossier $dossier)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('dossier_delete', array('id' => $dossier->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
