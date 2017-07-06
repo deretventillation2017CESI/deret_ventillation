26c4f3800995929921dadcdf4cb8651103ae7672
loic <bezzaouya.loic@hotmail.fr>
2017-07-06 16:12:37 +0200

    Commit :  - Gestion utilisateur

--- src/AppBundle/Form/UtilisateurType.php
+++ src/AppBundle/Form/UtilisateurType.php
@@ -5,6 +5,9 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use FOS\UserBundle\Util\LegacyFormHelper;
+
 
 class UtilisateurType extends AbstractType
 {
@@ -32,7 +35,16 @@ class UtilisateurType extends AbstractType
             ->add('typeHoraire')
             ->add('secteur')
             ->add('batiment')
-            ->add('poste');
+            ->add('poste')
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ));
     }
 
     /**
